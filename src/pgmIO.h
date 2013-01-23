4a085410fd32f60abc0b5bdc3e03e8aec0a732bc
Denis Ogun <do1303@my.bristol.ac.uk>
2013-01-23 21:05:03 +0000

    Update src/pgmIO.h

--- src/pgmIO.h
+++ src/pgmIO.h
@@ -5,9 +5,6 @@
 #include <stdio.h>
 #include <string.h>
 
-int _writepgm(unsigned char x[], int height, int width, char fname[]);
-int _readpgm(unsigned char x[], int height, int width, char fname[]);
-
 int _openinpgm(char fname[], int width, int height);
 int _readinline(unsigned char line[], int width);
 int _closeinpgm();
